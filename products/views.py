bd4850929b098afdb84ff0feb69a1b027237fd66
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-01-09 14:27:18 +0000

    feat: add product sort/select

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from .models import Category, Product
 
@@ -9,34 +10,48 @@ def all_products(request):
     Show the all products page.
 
     Show all products by default.
-    If the user submitted a search, only show matching products.
-    If the search term is blank, show an error message.
-    If the user selected categories, only show matching products.
+    If the user submitted a search, only show matching products. If the search
+    term is blank, show an error message.
+    If the user selected "products by X", show sorted products.
+    If the user selected a category, only show matching products.
     """
 
     # Show all products by default
     search_term = None
     categories = None
+    sort = None
+    direction = None
     products = Product.objects.all()
 
-    # If the user submitted a product search, show matching products.
-    # If the search was empty, show an error message.
     if request.GET:
-        is_product_search = "q" in request.GET
-        if is_product_search:
+
+        # If user submitted a product search, show matching products. If the
+        # search term is empty, show an error message.
+        if "q" in request.GET:
             search_term = request.GET["q"]
             if search_term:
-                query = Q(name__icontains=search_term) | Q(
-                    description__icontains=search_term
+                products = products.filter(
+                    Q(name__icontains=search_term)
+                    | Q(description__icontains=search_term)
                 )
-                products = products.filter(query)
             else:
                 messages.error(request, "You didn't enter any search text!")
                 return redirect(reverse("products"))
 
-        # If the user selected categories, show matching products.
-        is_category_search = "category" in request.GET
-        if is_category_search:
+        # If user selected "products by X", show sorted products.
+        if "sort" in request.GET:
+            sort = request.GET["sort"]
+            sortkey = sort
+            # If sorting by alpha field, use lowercase sort
+            if sort == "name":
+                sortkey = "lower_name"
+                products = products.annotate(lower_name=Lower("name"))
+            if request.GET.get("direction") == "desc":
+                sortkey = f"-{sortkey}"
+            products = products.order_by(sortkey)
+
+        # If user selected a category, show matching products.
+        if "category" in request.GET:
             category_names = request.GET["category"].split(",")
             products = products.filter(category__name__in=category_names)
             categories = Category.objects.filter(name__in=category_names)
@@ -45,7 +60,8 @@ def all_products(request):
     context = {
         "products": products,
         "search_term": search_term,
-        "categories": categories,
+        "current_categories": categories,
+        "current_sorting": f"{sort}_{direction}",
     }
     return render(request, "products/products.html", context)
 
