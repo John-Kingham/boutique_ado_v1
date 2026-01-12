0e659d325fd7b6f9a8b99278a922537f561f3daa
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-01-12 14:35:26 +0000

    feat: add add-to-bag feature

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def view_bag(request):
-    """A view to render the bag contents page."""
+    """Render the bag contents page."""
     return render(request, "bag/bag.html")
+
+
+def add_to_bag(request, item_id):
+    """Add item(s) to bag and refresh the product detail page."""
+    quantity = int(request.POST.get("quantity"))
+    bag = request.session.get("bag", {})
+    if item_id not in bag:
+        bag[item_id] = 0
+    bag[item_id] += quantity
+    request.session["bag"] = bag
+    return redirect(request.POST.get("redirect_url"))
