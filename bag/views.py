e4ccbebf0a5b59bb80ffbee36fe028f13a18ade0
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-01-15 10:27:48 +0000

    feat: add update/remove and subtotal for bag items

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import HttpResponse, redirect, render, reverse
 
 
 def view_bag(request):
@@ -7,27 +7,65 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """Add item to bag and refresh the product detail page."""
+    """Add item to the bag and refresh the product detail page."""
     bag = request.session.get("bag", {})
     quantity = int(request.POST.get("quantity"))
     size = request.POST.get("product_size")
     if size:
-        update_bag_with_sized_items(item_id, bag, quantity, size)
+        _update_bag_with_sized_items(item_id, bag, quantity, size)
     else:
-        update_bag_with_unsized_items(item_id, bag, quantity)
-    request.session["bag"] = bag
+        _update_bag_with_unsized_items(item_id, bag, quantity)
+    request.session.modified = True
     return redirect(request.POST.get("redirect_url"))
 
 
-def update_bag_with_unsized_items(item_id, bag, quantity):
-    """Update bag with items that don't have a size."""
+def adjust_bag(request, item_id):
+    """Adjust the quantity of an item in the bag."""
+    bag = request.session.get("bag")
+    quantity = int(request.POST.get("quantity"))
+    size = request.POST.get("product_size")
+    if size:
+        if quantity > 0:
+            bag[item_id]["items_by_size"][size] = quantity
+        else:
+            del bag[item_id]["items_by_size"][size]
+            if not bag[item_id]["items_by_size"]:
+                del bag[item_id]
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            del bag[item_id]
+    request.session.modified = True
+    return redirect(reverse("view_bag"))
+
+
+def remove_from_bag(request, item_id):
+    """Remove an item from the bag."""
+    try:
+        bag = request.session.get("bag")
+        size = request.POST.get("product_size")
+        if size:
+            del bag[item_id]["items_by_size"][size]
+            if not bag[item_id]["items_by_size"]:
+                del bag[item_id]
+        else:
+            del bag[item_id]
+        request.session.modified = True
+        return HttpResponse(status=200)
+    except Exception:
+        return HttpResponse(status=500)
+
+
+def _update_bag_with_unsized_items(item_id, bag, quantity):
+    """Update the bag with items that don't have a size."""
     if item_id not in bag:
         bag[item_id] = 0
     bag[item_id] += quantity
 
 
-def update_bag_with_sized_items(item_id, bag, quantity, size):
-    """Update bag with items that have a size."""
+def _update_bag_with_sized_items(item_id, bag, quantity, size):
+    """Update the bag with items that have a size."""
     if item_id not in bag:
         bag[item_id] = {"items_by_size": {size: 0}}
     if size not in bag[item_id]["items_by_size"]:
