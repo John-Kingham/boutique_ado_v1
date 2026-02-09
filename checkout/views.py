e98c2c5130b773cbbf8cf0b9588fe09681d1d2cf
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-02-09 11:49:07 +0000

    feat: add order history and update checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,11 @@ from django.shortcuts import (
 from django.views.decorators.http import require_POST
 import stripe
 
+from bag.contexts import bag_contents
+from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+
 from .checkout_messages import (
     empty_bag_error_message,
     form_error_message,
@@ -22,8 +27,6 @@ from .checkout_messages import (
 )
 from .forms import OrderForm
 from .models import Order, OrderLineItem  # noqa: F401
-from bag.contexts import bag_contents
-from products.models import Product
 
 
 def checkout(request):
@@ -35,8 +38,32 @@ def checkout(request):
     if bag_is_empty:
         messages.error(request, empty_bag_error_message())
         return redirect(reverse("products"))
+
+    # Initialise order form
+    order_form = None
+    if request.user.is_authenticated:
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            order_form = OrderForm(
+                initial={
+                    "full_name": profile.user.get_full_name(),
+                    "email": profile.user.email,
+                    "phone_number": profile.default_phone_number,
+                    "country": profile.default_country,
+                    "postcode": profile.default_postcode,
+                    "town_or_city": profile.default_town_or_city,
+                    "street_address1": profile.default_street_address1,
+                    "street_address2": profile.default_street_address2,
+                    "county": profile.default_county,
+                }
+            )
+        except UserProfile.DoesNotExist:
+            order_form = OrderForm()
+    else:
+        order_form = OrderForm()
+
     context = {
-        "order_form": OrderForm(),
+        "order_form": order_form,
         "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
         "client_secret": _payment_intent(request).client_secret,
     }
@@ -61,7 +88,7 @@ def _save_order(request):
     Return (HTTPRedirectResponse): Redirect user to the next page.
     """
     bag = request.session.get("bag", {})
-    order_form = OrderForm(_form_data(request))
+    order_form = OrderForm(_order_form_data(request))
     if order_form.is_valid():
         order = order_form.save(commit=False)
         pid = request.POST.get("client_secret").split("_secret")[0]
@@ -103,7 +130,7 @@ def _add_order_line_item(order, item_id, item_data):
             order_line_item.save()
 
 
-def _form_data(request):
+def _order_form_data(request):
     """Return a dict of form data extracted from the post request."""
     return {
         "full_name": request.POST["full_name"],
@@ -120,8 +147,30 @@ def _form_data(request):
 
 def checkout_success(request, order_number):
     """Handle successful checkouts."""
-    # save_info = request.session.get("save_info")
+    save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        # Attach the user's profile to the order
+        profile = UserProfile.objects.get(user=request.user)
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                "default_phone_number": order.phone_number,
+                "default_country": order.country,
+                "default_postcode": order.postcode,
+                "default_town_or_city": order.town_or_city,
+                "default_street_address1": order.street_address1,
+                "default_street_address2": order.street_address2,
+                "default_county": order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, order_success_message(order))
     request.session.pop("bag", None)
     return render(request, "checkout/checkout_success.html", {"order": order})
