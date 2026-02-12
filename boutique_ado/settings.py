a77eed0c6e457e5106892ff5af2f802c662f533a
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-02-12 11:54:00 +0000

    fix: fix incorrect module name in Procfile

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,12 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-7_i6th*jegadnd5m9y%3cq5e7^ed@7=)078+azdjask_!dsmm@"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = [
     "127.0.0.1",  # VS Code previews
