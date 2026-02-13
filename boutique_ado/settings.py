02ad1fd443235b9f2877c658b450d0076e7ae53f
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-02-13 14:29:14 +0000

    fix: fix typo in EMAIL_BACKEND setting

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -222,7 +222,7 @@ if "DEVELOPMENT" in os.environ:
     DEFAULT_FROM_EMAIL = "boutiqueado@example.com"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
-    EMAIL_BACKEND = "django.core.mail.backends.smpt.EmailBackend"
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = "smtp.gmail.com"
